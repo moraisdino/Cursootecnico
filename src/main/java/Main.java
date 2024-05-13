8b2c879eaa4e8fbeb510e00338ee4a8fe334ef2e
José Gabriel Morais <169068802+moraisdino@users.noreply.github.com>
2024-05-13 22:27:41 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("###########################################");
+    System.out.println("#O meu curso de desenvolvimento de sistema#");
+    System.out.println("###########################################");
+    System.out.println("");
+System.out.println("olá me chamo josé e vim falar sobre o meu curso tecnico,no começo de 2024 pensei muito sobre o curso,pensava que seria dificil ou ate chato,mas vi que aprender sobre softwares e muito legal,porém trabalhoso!! Somos uma sala de 40 alunos,nos divertimos e estudamos juntos!");
+    System.out.println("");
+System.out.println("Temos aula com o querido Juliano,um professor baixinho,gente boa,que ensina muito bem!Por outro lado vem o Stasiak,que é um homem alto,com barba,engraçado e bem rigído,ele ensina bem e as vezes faz umas belas piadas!!");
+    System.out.println("");
+    System.out.println("Pensava que as novas materias seriam dificil,porem agora pelo ao contrario,vejo que nao será tão dificil passar nas novas materias,no começo pegou a gente de jeito,principalmente com a tabela verdade,mas aprendemos muito bem e é bem legal!");
+    System.out.println("");
+    System.out.println("Cheguei a conclusão que esse curso vale a pena,apesar de algumas dificuldades podemos seguir forte e mostrar que ano perdemos tempo!Só tenho que agradecer aos ensinamentos dos professores e pela paciencia deles! na minha conclusao não sera tão dificil e sim um modo normal. ");
+  }
+
+}
